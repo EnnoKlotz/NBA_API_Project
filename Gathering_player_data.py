f1813af6ebe9560cdffcc45353bb299b88cc5bae
EnnoKlotz <Enno.klotz@gmail.com>
2025-01-22 12:44:14 -0800

    Seperating functions and gathering player data

--- Gathering_player_data.py
+++ Gathering_player_data.py
@@ -1,4 +1,12 @@
 from nba_api_functions import nba_playerid_by_season
+from nba_api_functions import get_player_experience
+from nba_api_functions import get_home_and_away_games
+from nba_api_functions import get_player_team_data
+import os 
+
+# Setting working directory 
+new_dir = "/Users/ennok./Documents/GitHub/NBA_API_Project" 
+os.chdir(new_dir)
 
 #Calling the player_id function 
 player_ids = nba_playerid_by_season(season='2023-24', output_csv='nba_players_2023_24.csv')
@@ -6,23 +14,21 @@ player_ids = nba_playerid_by_season(season='2023-24', output_csv='nba_players_20
 
 
 #Calling the player experance function 
-player_experience = get_player_experience(player_ids)
+#player_experience = get_player_experience(player_ids)
 
 #calling the home & away games function 
-home_games_df, away_games_df = get_home_and_away_games(
-    player_ids, 
-    save_csv=True, 
-    home_csv="all_home_games.csv", 
-    away_csv="all_away_games.csv"
-)
+# home_games_df, away_games_df = get_home_and_away_games(
+#     player_ids, 
+#     save_csv=True, 
+#     home_csv="all_home_games.csv", 
+#     away_csv="all_away_games.csv"
+# )
 
 
 
 # Example usage
 player_team_dict = get_player_team_data(player_ids[:5], output_csv='player_team_data.csv')
 print(player_team_dict)
-
-
 
 
 
