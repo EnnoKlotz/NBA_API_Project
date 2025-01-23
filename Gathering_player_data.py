01b63a868cee9ac6096bb5ca605ee6bb845b8db6
EnnoKlotz <Enno.klotz@gmail.com>
2025-01-23 14:22:12 -0800

    Player Team Data downloaded

--- Gathering_player_data.py
+++ Gathering_player_data.py
@@ -23,25 +23,44 @@ unique_playerids = set(list_of_ids)
 
 unique_playerids = list(unique_playerids)
 
-player_ids_df = pd.DataFrame(unique_playerids, columns=['Player ID'])
-
+#player_ids_df = pd.DataFrame(unique_playerids, columns=['Player ID'])
 #player_ids_df.to_csv("all_players_from_2015-2024.csv")
 
 
 player_team_data = pd.DataFrame(columns=['Player ID', 'Season', 'Team ID'])
 
-# Getting player_id, season_id, and team_id
-for i in seasons_of_interest[:2]:  
-    # Getting the players for the given season 
-    players = nba_playerid_by_season(season= i, output_to_csv= False)
-    # Converting them to strings
-    str_players = [str(x) for x in players]
-    
-    for j in str_players[:4]:  
-        # Getting player team data
-        team_df = get_player_team_data(player_ids=[j], save_to_csv=False)
-        # Adding team data to a csv
-        player_team_data = pd.concat([player_team_data, team_df], ignore_index= True)
-        
-player_team_data = player_team_data[['Player ID', 'Season ID', 'Team ID', 'Team Abbreviation']]
+# # Getting player_id, season_id, and team_id 
+# for i in unique_playerids:  
+#     # Getting player team data
+#     team_df = get_player_team_data(player_ids=[i], save_to_csv=False)
+#     # Adding team data to a csv
+#     player_team_data = pd.concat([player_team_data, team_df], ignore_index= True)
+# #Removing 2024-25 season
+# player_team_data = player_team_data.loc[player_team_data['Season ID'] != '2024-25']       
+# player_team_data = player_team_data[['Player ID', 'Season ID', 'Team ID', 'Team Abbreviation']]
+
+# #Saving temp progress
+# player_team_data.to_csv("player_team_data_temp_checj_point.csv", index= False)
+
+#Intialize empty df
+player_exper_data = pd.DataFrame(columns=['Player ID', 'Season ID', 'Experience'])
+# Getting player experance 
+for i in unique_playerids: 
+    print(f"Working on player id: {i}")
+    for j in seasons_of_interest:
+        # Get current player experance
+        current_player = get_player_experience(player_ids= [i], season_ids= [j], save_to_csv= False)
+        player_exper_data = pd.concat([player_exper_data, current_player], ignore_index= True)
+# Save progress        
+player_exper_data.to_csv("experance_data_checkpoint.csv", index= False)
+
+# Merging experance and team history 
+#merged_df = pd.merge(player_team_data, player_exper_data, on=['Player ID', 'Season ID'], how='left')
+
+#merged_df.to_csv("Player Team and Experance 2014-2024.csv", index= False)
+#print("Player Team and Experance 2014-2024.csv has been printed!")
+
+
+
+
 
